24715c3c9cfd5879f1727fc98b72bf19a777999a
kencal20 <kenadjei20@gmail.com>
2023-05-24 19:41:27 +0000

    heading ,footer and split components creation

---
+++ components/footer.js
@@ -0,0 +1,51 @@
+export default function footerComponent(){
+    return `
+    <footer>
+
+    <div class="col">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <div id="nav"></div>
+    </div>
+
+    <div class="col">
+        <a href="https://goo.gl/maps/K5RHNDMgixjgEDUQ9 " target="_blank"><i class="bi bi-geo-alt-fill icon"
+                target="_blank">
+            </i></a>
+        <h3>LA-TSEADDO<br>P.O.BOX 629 LA-ACCRA</h3>
+        <h3>CONTACT</h3>
+        <div class="row">
+            <div class="col">
+                <i class="bi bi-facebook icon"></i>
+            </div>
+            <div class="col">
+                <i class="bi bi-youtube icon"></i>
+
+            </div>
+            <div class="col">
+                <i class="bi bi-instagram icon"></i>
+            </div>
+
+
+        </div>
+        <div class="row">
+            <i class="bi bi-envelope-fill ">PromiselandSDAC@gmail.com</i>
+            <i class="bi bi-telephone">+233(0)209037926</i>
+            <i class="bi bi-whatsapp">+233(0)209037926</i>
+
+
+
+        </div>
+        <h6> Seventh-Day Adventist Church <br> Promise Land <br> All rights reserved &copy; <i id="year">
+            </i>
+            <br>
+
+
+        </h6>
+
+
+    </div>
+</footer>
+    `
+}
---
+++ components/split.js
@@ -0,0 +1,25 @@
+export default function splitComponent() {
+    return ` 
+    <section>
+    <div class="split left">
+        <h1>MORE ABOUT US AS A LOCAL CHURCH</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor voluptate alias odio, sint rem
+            consequuntur cupiditate ullam ipsam voluptates odit blanditiis dolorem neque expedita saepe
+            officiis!
+            Suscipit facilis ipsum nemo?</p>
+    
+    </div>
+    <div class="split right">
+        <h1>MORE ABOUT US AS A LOCAL CHURCH</h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut ipsum consectetur voluptates officiis
+            consequuntur ab labore, accusantium sequi non nulla? Delectus fugiat alias rem ratione voluptas,
+            velit
+            nesciunt rerum? Error.
+        </p>
+    </div>
+    </div>
+
+</section>
+    `
+}
